baa7f3dd5d5fbcebe042c3c3933803b2eae8683f
David Gomez <davdg25@gmail.com>
2019-11-19 13:20:06 -0700

    Created testing and functionality for editing and updating a shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -18,6 +18,23 @@ class SheltersController < ApplicationController
     redirect_to '/shelters'
   end
 
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    @shelter = Shelter.find(params[:id])
+    @shelter.update({
+                     name: params[:name],
+                     address: params[:address],
+                     city: params[:city],
+                     state: params[:state],
+                     zip: params[:zip]
+      })
+    @shelter.save
+    redirect_to "/shelters/#{@shelter.id}"
+  end
+
 ########### PRIVATE METHODS ###########
 
   def shelter_params

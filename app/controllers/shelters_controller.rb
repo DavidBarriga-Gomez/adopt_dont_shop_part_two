99b2c533429089288ffa10a20f892930139cefee
David Gomez <davdg25@gmail.com>
2019-11-19 12:03:20 -0700

    finished previous work and created form functionality and testing for a new shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,4 +7,21 @@ class SheltersController < ApplicationController
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    Shelter.create(shelter_params)
+
+    redirect_to '/shelters'
+  end
+
+########### PRIVATE METHODS ###########
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
+
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,7 @@
 class Shelter < ApplicationRecord
-
-end 
+  validates_presence_of :name,
+                        :address,
+                        :city,
+                        :state,
+                        :zip
+end

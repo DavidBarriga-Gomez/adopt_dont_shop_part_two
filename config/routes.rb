ed6b591904d9ad6bd7d3fd7e1af1fffd6f0849fe
David Gomez <davdg25@gmail.com>
2019-11-20 23:12:39 -0700

    Add routes for creating a new adoptable pet

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+
   get '/shelters/:shelter_id/pets', to: 'pets#shelter_pets_index'
+  get '/shelters/:shelter_id/pets/new', to: 'pets#new'
+  post '/shelters/:shelter_id/pets', to: 'pets#create'
 
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'

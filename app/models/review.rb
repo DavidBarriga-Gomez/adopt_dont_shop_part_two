70ff10acfef79dc40de2ddd2824fc7f26f4f8b83
David Gomez <davdg25@gmail.com>
2019-12-03 16:09:37 -0700

    remove validates_presence_of :picture

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,9 @@
    belongs_to :shelter
    validates_presence_of :title,
                         :rating,
-                        :content,
-                        :picture
+                        :content
+                        # :picture
+  # some_value :picture
+  # validates :picture, :presence => false, :allow_blank => true
+  # validates :picture, :presence => true, :uniqueness => {:allow_blank => true}
  end

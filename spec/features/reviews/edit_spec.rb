56d8a60d6f35b64f4273f9ec3709da9d057fe8ae
David Gomez <davdg25@gmail.com>
2019-12-04 21:56:28 -0700

    feature testing for edit review form to show a flash message

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -14,44 +14,58 @@ RSpec.describe 'visit /shelters/:id show page', type: :feature do
   it 'completely functional edit link next each shelter review with a prepopulated form ' do
     visit "/shelters/#{@raccoon_shelter.id}"
 
-      within "#review-#{@raccoon_shelter_review_1.id}" do
-        expect(page).to have_link('Edit Review')
+    # within "#review-#{@raccoon_shelter_review_1.id}" do
+    #   expect(page).to have_link('Edit Review')
+    #
+    #   click_on('Edit Review')
+    # end
+    # expect(current_path).to eq("/shelters/#{@raccoon_shelter.id}/reviews/#{@raccoon_shelter_review_1.id}/edit")
+    #
+    # click_on 'Submit'
+    #
+    # expect(current_path).to eq("/shelters/#{@raccoon_shelter.id}")
+    #
+    # expect(page).to have_content(@raccoon_shelter_review_1.title)
+    # expect(page).to have_content(@raccoon_shelter_review_1.rating)
+    # expect(page).to have_content(@raccoon_shelter_review_1.content)
+    # expect(page).to have_css("img[src*='https://i.dailymail.co.uk/i/pix/2017/11/14/16/4658323800000578-0-image-a-2_1510676236619.jpg']")
 
-        click_on('Edit Review')
-      end
-        expect(current_path).to eq("/shelters/#{@raccoon_shelter.id}/reviews/#{@raccoon_shelter_review_1.id}/edit")
+    within "#review-#{@raccoon_shelter_review_1.id}" do
+      click_on('Edit Review')
+    end
 
-        save_and_open_page
-        click_on 'Submit'
+   expect(current_path).to eq("/shelters/#{@raccoon_shelter.id}/reviews/#{@raccoon_shelter_review_1.id}/edit")
 
-        expect(current_path).to eq("/shelters/#{@raccoon_shelter.id}")
+    title = 'New Title'
+    rating = 2
+    content = 'New Content'
+    picture = 'https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/03/sei_54738239-dee9.jpg?quality=90&strip=all&zoom=1&resize=644%2C428&ssl=1'
 
-        expect(page).to have_content(@raccoon_shelter_review_1.title)
-        expect(page).to have_content(@raccoon_shelter_review_1.rating)
-        expect(page).to have_content(@raccoon_shelter_review_1.content)
-        expect(page).to have_css("img[src*='https://i.dailymail.co.uk/i/pix/2017/11/14/16/4658323800000578-0-image-a-2_1510676236619.jpg']")
+    fill_in :title, with: title
+    fill_in :rating, with: rating
+    fill_in :content, with: content
+    fill_in :picture, with: picture
 
-      within "#review-#{@raccoon_shelter_review_1.id}" do
-        click_on('Edit Review')
-      end
+    click_on('Submit')
 
-        title = 'New Title'
-        rating = 2
-        content = 'New Content'
-        picture = 'https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/03/sei_54738239-dee9.jpg?quality=90&strip=all&zoom=1&resize=644%2C428&ssl=1'
+    expect(current_path).to eq("/shelters/#{@raccoon_shelter.id}")
 
-        fill_in :title, with: title
-        fill_in :rating, with: rating
-        fill_in :content, with: content
-        fill_in :picture, with: picture
-
-        click_on('Submit')
-
-        expect(current_path).to eq("/shelters/#{@raccoon_shelter.id}")
-
-        expect(page).to have_content(title)
-        expect(page).to have_content(rating)
-        expect(page).to have_content(content)
-        expect(page).to have_css("img[src*='https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/03/sei_54738239-dee9.jpg?quality=90&strip=all&zoom=1&resize=644%2C428&ssl=1']")
+    expect(page).to have_content(title)
+    expect(page).to have_content(rating)
+    expect(page).to have_content(content)
+    expect(page).to have_css("img[src*='https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/03/sei_54738239-dee9.jpg?quality=90&strip=all&zoom=1&resize=644%2C428&ssl=1']")
   end
+
+    it 'If title, rating, or content are not filled in there is a flash message and a redirect to review edit form' do
+      visit "/shelters/#{@raccoon_shelter.id}"
+
+      within "#review-#{@raccoon_shelter_review_1.id}" do
+        click_on('Edit Review')
+      end
+
+      click_on 'Submit'
+
+      expect(page).to have_content('Review Not Created! Make Sure To Fill In Title, Rating, And Content!')
+      expect(page).to have_button('Submit')
+    end
 end
